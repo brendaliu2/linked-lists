773fb189e6cd336d489496931aa2e5f522552d3c
Brenda Liu <brenda.liu24@gmail.com>
2022-09-19 13:51:26 -0500

    finish push, unshift, pop linked list

--- linked-list.js
+++ linked-list.js
@@ -23,18 +23,70 @@ class LinkedList {
   /** push(val): add new value to end of list. */
 
   push(val) {
+    //make new node
+    const newNode = new Node(val);
+    //if empty list, set newnode as head and tail
+    if (!this.length) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      //set tail next to new node
+      this.tail.next = newNode;
+      //set this.tail to new node
+      this.tail = newNode;
+    }
 
+    this.length++;
   }
 
   /** unshift(val): add new value to start of list. */
 
   unshift(val) {
+    //make new node
+    const newNode = new Node(val);
+    //if empty list, set newnode as head and tail
+    if (!this.length) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      //set new node next is head
+      newNode.next = this.head;
+      //new new is new head
+      this.head = newNode;
+
+    }
+    this.length++;
 
   }
 
   /** pop(): return & remove last item. */
 
   pop() {
+    //if length = 0 return undefined
+    if(!this.length) return undefined;
+    //if length = 1
+      //set head and tail to null
+      //return original head
+    if(this.length === 1){
+      const oldHead = this.head;
+      this.head = null;
+      this.tail = null
+      this.length = 0;
+      return oldHead.val;
+    } else {
+      //walk through list
+        //if curr.next is tail, set as new tail
+        //return old tail
+      let current = this.head;
+      while(current.next !== this.tail){
+        current = current.next;
+      }
+      const oldTail = this.tail;
+      this.tail = current;
+      current.next = null;
+      this.length--;
+      return oldTail.val;
+    }
 
   }
 
@@ -71,7 +123,7 @@ class LinkedList {
   /** average(): return an average of all values in the list */
 
   average() {
-    
+
   }
 }
 
